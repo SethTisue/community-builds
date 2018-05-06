7a9ec9aa3b182af4d5dc8926f1b421a182302e7a
Seth Tisue <seth@tisue.net>
2018-05-06 17:00:27 +0200

    wip

--- run.sh
+++ run.sh
@@ -21,7 +21,7 @@ function latestNightly() {
 }
 
 # Defaults
-scala_version_default="2.12.7-bin-689cd08"  # 1 May 2018
+scala_version_default="2.13.0-pre-ac5c8fa"  # 6 May 2018
 scala_version="$scala_version_default"
 root_dir=$(pwd)
 config_dir="configs"
